44d0777c5ed3dcc693fbf72734d0fe981ff692e9
Colin C <ciciccz@gmail.com>
2023-11-02 20:20:59 -0600

    updated the endpoint for GET using name. Cleaned query input

--- src/routes.js
+++ src/routes.js
@@ -53,8 +53,8 @@ module.exports.register = (app, database) => {
 
 
         try {
-            query = `SELECT * FROM item where name = ?`;            // SQL query
-            const results = await database.query(query, [itemName]); // Execute the query with the ID as a parameter
+            query = `SELECT * FROM item WHERE LOWER(name) = LOWER(?)`;            // SQL query
+            const results = await database.query(query, [itemName.trim()]); // Execute the query with the ID as a parameter
 
             if (results.length === 0) {
                 return res.status(404).send('Item not found').end();
