d5a1c4cc4ed4abc8301e66fe41b5b52dfefbaff7
Colin C <ciciccz@gmail.com>
2023-11-03 18:50:39 -0600

    refactoed some code into sendJson for GET

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,22 @@ module.exports.register = (app, database) => {
     });
 
     app.get('/api/item', async (req, res) => {
-        let query;
-        query = database.query('SELECT * FROM item');
 
-        const records = await query;
+        const itemName = req.query.name;
 
-        res.status(200).send(JSON.stringify(records)).end();
+        if (!itemName) {
+
+            query = database.query('SELECT * FROM item');
+
+            const records = await query;
+
+            res.status(200).send(JSON.stringify(records)).end();
+        }
+
+        query = `SELECT * FROM item WHERE LOWER(name) = LOWER(?)`
+
+        sendJson(query, itemName);
+
     });
 
     app.get('/api/item/:id', async (req, res) => {
@@ -23,47 +33,10 @@ module.exports.register = (app, database) => {
             return res.status(400).send('Item ID is required').end();
         }
 
-        let query;
+        let query = `SELECT * FROM item where id = ?`;
 
+        sendJson(query, itemId);
 
-        try {
-            query = `SELECT * FROM item where id = ?`;            // SQL query
-            const results = await database.query(query, [itemId]); // Execute the query with the ID as a parameter
-
-            if (results.length === 0) {
-                return res.status(404).send('Item not found').end();
-            }
-
-            res.status(200).send(JSON.stringify(results[0])).end(); // Send the first item found as a response
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('An error occurred while fetching the item').end();
-        }
-    });
-
-    app.get('/api/item', async (req, res) => {
-
-        const itemName = req.query.name;
-
-        if (!itemName) {
-            return res.status(400).send('Item name is required').end();
-        }
-
-        let query;
-
-        try {
-            query = `SELECT * FROM item WHERE LOWER(name) = LOWER(?)`;            // SQL query
-            const results = await database.query(query, [itemName]); // Execute the query with the ID as a parameter
-
-            if (results.length === 0) {
-                return res.status(404).send('Item not found').end();
-            }
-
-            res.status(200).send(JSON.stringify(results[0])).end(); // Send the first item found as a response
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('An error occurred while fetching the item').end();
-        }
     });
 
     app.post('/api/item', async (req, res) => {
@@ -115,3 +88,18 @@ module.exports.register = (app, database) => {
     }
     );
 };
+
+sendJson = async (query, param, errorMsg) => {
+
+    try {
+        const results = await database.query(query, param);
+
+        if (results.length === 0) {
+            return res.status(404).send('Item not found').end();
+        }
+
+        res.status(200).send(JSON.stringify(results[0])).end(); // Send the first item found as a response
+    } catch (error) {
+        res.status(500).send(error).end();
+    }
+}
